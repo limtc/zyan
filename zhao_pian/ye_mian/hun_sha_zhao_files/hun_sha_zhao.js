5e57f001b9f098aec25fcaf59dcdae6599f16e0e
limtc <thyechean@gmail.com>
2018-07-22 22:00:19 +0800

    Add files via upload

---
+++ zhao_pian/ye_mian/hun_sha_zhao_files/hun_sha_zhao.js
@@ -0,0 +1,29 @@
+// Created by iWeb 3.0.1 local-build-20100116
+
+function createMediaStream_id1()
+{return IWCreatePhotocast("file://localhost/Users/limtc/Sites/Zhao_Yan/zhao_pian/ye_mian/hun_sha_zhao_files/rss.xml",true);}
+function initializeMediaStream_id1()
+{createMediaStream_id1().load('file://localhost/Users/limtc/Sites/Zhao_Yan/zhao_pian/ye_mian',function(imageStream)
+{var entryCount=imageStream.length;var headerView=widgets['widget1'];headerView.setPreferenceForKey(imageStream.length,'entryCount');NotificationCenter.postNotification(new IWNotification('SetPage','id1',{pageIndex:0}));});}
+function layoutMediaGrid_id1(range)
+{createMediaStream_id1().load('file://localhost/Users/limtc/Sites/Zhao_Yan/zhao_pian/ye_mian',function(imageStream)
+{if(range==null)
+{range=new IWRange(0,imageStream.length);}
+IWLayoutPhotoGrid('id1',new IWPhotoGridLayout(3,new IWSize(169,169),new IWSize(169,0),new IWSize(203,184),27,27,0,new IWSize(23,25)),new IWPhotoFrame([IWCreateImage('hun_sha_zhao_files/Freestyle_01.png'),IWCreateImage('hun_sha_zhao_files/Freestyle_02.png'),IWCreateImage('hun_sha_zhao_files/Freestyle_03.png'),IWCreateImage('hun_sha_zhao_files/Freestyle_06.png'),IWCreateImage('hun_sha_zhao_files/Freestyle_09.png'),IWCreateImage('hun_sha_zhao_files/Freestyle_08.png'),IWCreateImage('hun_sha_zhao_files/Freestyle_07.png'),IWCreateImage('hun_sha_zhao_files/Freestyle_04.png')],null,2,0.566667,3.000000,3.000000,3.000000,3.000000,22.000000,24.000000,23.000000,25.000000,166.000000,222.000000,166.000000,222.000000,null,null,null,0.100000),imageStream,range,null,null,1.000000,{backgroundColor:'rgb(0, 0, 0)',reflectionHeight:100,reflectionOffset:2,captionHeight:100,fullScreen:0,transitionIndex:2},'../../Media/slideshow.html','widget1','widget2','widget3')});}
+function relayoutMediaGrid_id1(notification)
+{var userInfo=notification.userInfo();var range=userInfo['range'];layoutMediaGrid_id1(range);}
+function onStubPage()
+{var args=window.location.href.toQueryParams();parent.IWMediaStreamPhotoPageSetMediaStream(createMediaStream_id1(),args.id);}
+if(window.stubPage)
+{onStubPage();}
+setTransparentGifURL('../../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{IWRegisterNamedImage('comment overlay','../../Media/Photo-Overlay-Comment.png')
+IWRegisterNamedImage('movie overlay','../../Media/Photo-Overlay-Movie.png')
+loadMozillaCSS('hun_sha_zhao_files/hun_sha_zhaoMoz.css')
+NotificationCenter.addObserver(null,relayoutMediaGrid_id1,'RangeChanged','id1')
+fixAllIEPNGs('../../Media/transparent.gif');Widget.onload();initializeMediaStream_id1()
+performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
